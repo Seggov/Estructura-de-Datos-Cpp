fa3dab47dc5028e26ff6b78ef9e74350b0603104
Pedro Segovia <pedro.segovia@alumnos.ucn.cl>
2026-09-05 17:23:35 -0400

    estructura :

--- Punteros/Main.cpp
+++ Punteros/Main.cpp
@@ -1,13 +1,29 @@
 #include <iostream>
 
-
 using namespace std;
 
+int main()
+{
 
-int main(){
+    int v[10];
+    for (int i = 0; i < 10; i++)
+    {
 
-    cout << "Hola bienvenidos" << endl;
+        v[i] = 10 * (i + 1);
+    }
+
+    int *p; // VARIABLE de tipo Puntero
+    p = &v[7];
+    *p = 4;
+    int y = *p;
+
+    cout << "*v = " << *v << endl;
+    cout << " v = " << v << endl;
+    cout << "&v = " << &v << endl;
+    cout << "*p = " << *p << endl;
+    cout << " p = " << p << endl;
+    cout << "&p = " << &p << endl;
+    cout << " y = " << y << endl;
+    cout << "&y = " << &y << endl;
     return 0;
-
-
 }
